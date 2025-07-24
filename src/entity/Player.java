9735b531af71deac653b93da6ef4005bc2f36cf3
towny3 <nicholas.townshend3@gmail.com>
2025-07-24 13:15:23 +1000

    added sprite counter for player animations

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -67,6 +67,16 @@ public class Player extends Entity {
 			direction = "right";
 			x += speed;
 		}
+		
+		spriteCounter++;
+		if (spriteCounter > 10) {
+			if (spriteNum == 1) {
+				spriteNum = 2;
+			} else if (spriteNum == 2) {
+				spriteNum = 1;
+			}
+			spriteCounter = 0;
+		}
 	}
 	
 	public void draw(Graphics2D g2) {
@@ -77,16 +87,32 @@ public class Player extends Entity {
 		
 		switch(direction) {
 		case "up":
-			image = up1;
+			if (spriteNum == 1) {
+				image = up1;
+			} else {
+				image = up2;
+			}
 			break;
 		case "down":
-			image = down1;
+			if (spriteNum == 1) {
+				image = down1;
+			} else {
+				image = down2;
+			}
 			break;
 		case "left":
-			image = left1;
+			if (spriteNum == 1) {
+				image = left1;
+			} else {
+				image = left2;
+			}
 			break;
 		case "right":
-			image = right1;
+			if (spriteNum == 1) {
+				image = right1;
+			} else {
+				image = right2;
+			}
 			break;
 		}
 		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
